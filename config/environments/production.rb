d7cfa9cc891c90c21c65ce0b303fe22160309be8
yogione <smotheram@yahoo.com>
2011-03-13 12:05:48 -0400

    mar 13 1205

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ SampleApp::Application.configure do
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
+  Paperclip.options[:command_path] = "/usr/local/bin"
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
