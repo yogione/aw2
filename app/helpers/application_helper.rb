15752bb1eb0edd3ffd5eb6e5d7aca0cbd5a14914
yogione <smotheram@yahoo.com>
2011-03-14 21:08:29 -0400

    mar 14 908 pm

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,11 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+   def meta_description
+    return @meta_description = "autoworker retired autoworker GM employees " +
+         " Ford employees Toytoa employees Honda employees Chryssler employees"
+  end
   
   def logo
     image_tag("awlogo.png", :alt => "Autoworker", :class => "round")
