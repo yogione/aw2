9f4febaf669917b68eb0a01ad272adefc47e9767
yogione <smotheram@yahoo.com>
2011-03-16 21:30:26 -0400

    mar 16 930pm

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,7 @@ class PagesController < ApplicationController
      # @micropost = Micropost.new
       @story = Story.new
       @count1 = 1
+     # @stories = Story.find(:all, :include => :comments, :order => 'created_at DESC')
       @stories = Story.find(:all, :include => :comments, :order => 'created_at DESC')
       # @stories = Story.paginate(:page => params[:page])
       if signed_in?
